f2eaafb42434b486214d30d2413747c683d025b3
raunak299 <iamraunak299@gmail.com>
2024-07-24 23:23:13 +0530

    basic setup for rabbitmq

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
 
+const { createChannel } = require("./utils/messageQueue");
+
 const { sendBasicEmail } = require("./services");
 const setUpJobs = require("./utils/jobs");
 const ticketController = require("./controller/ticket-controller");
 
-const setUpAndStartServer = () => {
+const setUpAndStartServer = async () => {
   const app = express();
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+
+  const channel = await createChannel();
 
   app.post("/api/v1/tickets", ticketController.create);
 
